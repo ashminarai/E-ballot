063a425b192aa615cbfde24ee76f60d872d62382
ashminarai <85731866+ashminarai@users.noreply.github.com>
2023-10-04 16:50:48 +0000

    Delete Server directory

--- Server/models/constituency.model.js
+++
@@ -1,7 +0,0 @@
-const DataTypes = require('sequelize')
-
-module.exports = (sequelize, Sequelize) => {
-    return sequelize.define("constituency", {
-        name: DataTypes.STRING
-    })
-}
--- Server/routes/main.routes.js
+++
@@ -1,8 +0,0 @@
-const {getCounts} = require("../controllers/main.controller");
-module.exports = app => {
-    const router = require("express").Router();
-
-    router.get('/count', getCounts)
-
-    app.use('/api', router)
-}
